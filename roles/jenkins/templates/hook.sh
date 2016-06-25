0f8cf2d461ad514a85fa0b45060681d33ca5994e
Dylan Scott <dylan@dylanscott.com.au>
2016-06-25 12:31:14 +1000

    Add Nginx config

---
+++ roles/jenkins/templates/hook.sh
@@ -0,0 +1,27 @@
+YOUR_HOSTED_ZONE_HERE="Z30SNW8LEQ3DWD"
+
+case "$1" in
+  "clean_challenge")
+    altname="$2"
+    challenge_token="$3"
+    keyauth_hook="$4"
+
+    aws route53 change-resource-record-sets --hosted-zone-id $YOUR_HOSTED_ZONE_HERE --change-batch "{\"Changes\":[{\"Action\":\"DELETE\",\"ResourceRecordSet\":{\"Name\":\"_acme-challenge.${altname}\",\"Type\":\"TXT\",\"TTL\":60,\"ResourceRecords\":[{\"Value\":\"\\\"${keyauth_hook}\\\"\"}]}}]}"
+    ;;
+  "deploy_challenge")
+      altname="$2"
+      challenge_token="$3"
+      keyauth_hook="$4"
+
+      aws route53 change-resource-record-sets --hosted-zone-id $YOUR_HOSTED_ZONE_HERE --change-batch "{\"Changes\":[{\"Action\":\"UPSERT\",\"ResourceRecordSet\":{\"Name\":\"_acme-challenge.${altname}\",\"Type\":\"TXT\",\"TTL\":60,\"ResourceRecords\":[{\"Value\":\"\\\"${keyauth_hook}\\\"\"}]}}]}"
+      sleep 20
+    ;;
+  "deploy_cert")
+    domain="$2"
+    private_key="$3"
+    cert="$4"
+    chain="$5"
+
+    # NO-OP
+    ;;
+esac
